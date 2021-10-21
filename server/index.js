267853bbd64b73a4ecdba63db1999fb4e0bc3f12
Bill Tiffany <daganlegalservice@gmail.com>
2021-10-21 14:12:49 -0600

    pushing up beginning code

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,"../index.html"))
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+  })
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.js'))
+  })
+const port = process.env.PORT || 4242;
+
+app.listen(port, ()=>console.log("The answer is 4242"));
